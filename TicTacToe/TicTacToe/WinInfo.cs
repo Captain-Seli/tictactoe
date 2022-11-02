5079ca649ef06570f977d382a051bb493aa6d787
Jon <47305276+Captain-Seli@users.noreply.github.com>
2022-11-02 12:39:23 -0400

    C# practice following OttoCodeBot's Tutorial on YT.

---
+++ TicTacToe/TicTacToe/WinInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class WinInfo
+    {
+        public WinType Type { get; set; } // Stores the type of win: row, col, or diag.
+        public int Number { get; set; } // Stores which row, col, or diag has won.
+    }
+}
